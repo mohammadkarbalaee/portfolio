2a6e63cd55a7899094eac4b08f4864143b059bde
Mohammad Karbalaee <muhammad.ksht@gmail.com>
2023-09-05 21:38:23 +0200

    update

--- src/lib/pages/apps/app.dart
+++ src/lib/pages/apps/app.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
     return Padding(
